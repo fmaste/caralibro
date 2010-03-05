16c20cc62935cde99bb2c5f77f097647abe8c5f0
fmaste <fmaste@users.noreply.github.com>
2010-03-05 03:50:46 -0200

    Added a remove comment method!

--- src/caralibro/dao/CommentDao.java
+++ src/caralibro/dao/CommentDao.java
@@ -18,6 +18,9 @@ import caralibro.model.constants.Facebook;
 // If there are no comments returns null
 public class CommentDao {
 
+	// Comment request response example
+	// Request: http://api.facebook.com/restserver.php?v=1.0&api_key=060c9d27db80e7bc1dab1f3ec1e48f63&ss=true&method=Stream.getComments&format=json&post_id=1443735325_155668449205&call_id=1267683315247&session_key=7a85a2cbaf58c823c450a95d-1443735325&sig=56afb28d2d3aa042f4ac2dba6fc885f5
+	// Response: [{"fromid":1062262331,"time":1255708755,"text":"No me quiero imaginar cuan al pedo estarias para ponerte con el facebook.. es la primera vez q apareces aca papaaaaa!! como andas??!!!","id":"1443735325_155668449205_6025124"},{"fromid":1443735325,"time":1255709295,"text":"Lo uso para jugar a esta porqueria que soy malisimo! jajaja","id":"1443735325_155668449205_6025449"}]
 	public static Collection<Comment> getFromPost(Application application, Session session, Post post) throws Exception {
 		Map<String,String> params = RequestFactory.create(application, session, "Stream.getComments");
 		params.put("post_id", post.getId());
@@ -31,15 +34,28 @@ public class CommentDao {
 		Collection<Comment> comments = new ArrayList<Comment>();
 		JSONArray commentsJsonArray = new JSONArray(commentsJsonResponse);
 		for (int i = 0; i < commentsJsonArray.length(); i++) {
+			// The comment index is retrieved as a String and CommentFactory must now how to handle it!
 			String commentString = commentsJsonArray.optString(i);
 			if (commentString != null && !commentString.isEmpty()) {
 				Comment comment = CommentFactory.create(commentString);
-				if (comment != null ) {
+				if (comment != null) {
 					comments.add(comment);
 				}
 			}			
 		}
 		return comments;
+	}
+	
+	public static boolean remove(Application application, Session session, Comment comment) throws Exception {
+		Map<String,String> params = RequestFactory.create(application, session, "Stream.removeComment");
+		params.put("comment_id", comment.getId());
+		RequestFactory.sign(params, application, session);
+		String response = Rest.makeRequest(Facebook.REST_SERVER, params);
+		if (response != null && !response.isEmpty() && response.equals("true")) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 	
 }
